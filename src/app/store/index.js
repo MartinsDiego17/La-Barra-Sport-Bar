3bc7caea1deaf6c336fd074d626ee82a35ff79c1
Diego <martinsdiego0513@outlook.es>
2024-06-24 10:00:09 -0300

    Log route

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -9,9 +9,8 @@ export const useStoreProducts = create((set) => ({
     getAllProducts: async () => {
         try {
             const url = process.env.NEXT_PUBLIC_GET_PRODUCTS;
-            console.log("Funciona arriba");
+            console.log("La ruta obtenida es: ", url);
             const { data } = await axios(url);
-            console.log("Lista de productos: ", data);
             return data;
         } catch (error) {
             console.error("ERROR: ", error.message);
